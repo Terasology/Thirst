012fdbc898c7cfa355bdeeda66c9a520103a2037
ktksan <kartikeya.sanwal@gmail.com>
2020-06-09 13:35:53 +0530

    typo

--- src/main/java/org/terasology/thirst/event/AffectThirstEvent.java
+++ src/main/java/org/terasology/thirst/event/AffectThirstEvent.java
@@ -18,7 +18,7 @@ package org.terasology.thirst.event;
 import org.terasology.entitySystem.event.AbstractValueModifiableEvent;
 
 /**
- * This event is sent out byt the {@link org.terasology.thirst.ThirstAuthoritySystem} to allow for other systems to
+ * This event is sent out by the {@link org.terasology.thirst.ThirstAuthoritySystem} to allow for other systems to
  * modify thirst decay.
  */
 public class AffectThirstEvent extends AbstractValueModifiableEvent {
