cf9844143713d7a42ffc920cfaa210f00412fb5f
ktksan <kartikeya.sanwal@gmail.com>
2020-06-13 20:01:49 +0530

    Fix AffectThirst

--- src/main/java/org/terasology/thirst/ThirstAuthoritySystem.java
+++ src/main/java/org/terasology/thirst/ThirstAuthoritySystem.java
@@ -71,6 +71,7 @@ public class ThirstAuthoritySystem extends BaseComponentSystem {
     private DelayManager delayManager;
 
     private boolean destroyDrink;
+    private float expectedDecay;
 
     /**
      * The interval (in milliseconds) at which healthDecreaseAmount (thirstComponent) is applied to the component.
@@ -223,15 +224,16 @@ public class ThirstAuthoritySystem extends BaseComponentSystem {
      */
     @ReceiveEvent(components = {ThirstComponent.class})
     public void characterMoved(CharacterMoveInputEvent event, EntityRef character, ThirstComponent thirst) {
-        final float expectedDecay = event.isRunning() ? thirst.sprintDecayPerSecond : thirst.normalDecayPerSecond;
+        expectedDecay = event.isRunning() ? thirst.sprintDecayPerSecond : thirst.normalDecayPerSecond;
+        AffectThirstEvent affectThirstEvent = new AffectThirstEvent(expectedDecay);
+        character.send(affectThirstEvent);
+        expectedDecay = affectThirstEvent.getResultValue();
         if (expectedDecay != thirst.waterDecayPerSecond) {
             // Recalculate current thirst and apply new decay
             thirst.lastCalculatedWater = ThirstUtils.getThirstForEntity(character);
             thirst.lastCalculationTime = time.getGameTimeInMs();
             // Send event to allow for other systems to modify thirst decay.
-            AffectThirstEvent affectThirstEvent = new AffectThirstEvent(expectedDecay);
-            character.send(affectThirstEvent);
-            thirst.waterDecayPerSecond = affectThirstEvent.getResultValue();
+            thirst.waterDecayPerSecond = expectedDecay;
             character.saveComponent(thirst);
         }
     }
